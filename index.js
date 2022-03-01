de6b6a068135a7e564887a125f4b4c57e10166cf
mkmeryk <inq@meryk.ca>
2022-03-01 12:38:23 -0800

    sign in page

--- index.js
+++ index.js
@@ -2,6 +2,30 @@
 const express = require('express');
 const app = express();
 
+//url encoder
+app.use(express.urlencoded({extended:true}));
+
+//cookie parser
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+//custom middleware
+app.use((req,res,next) =>{
+
+    const username = req.cookies.username;
+    res.locals.username = "";
+    if (username){
+
+        res.locals.username = username;
+        console.log(`signed in as ${username}`);
+
+    }
+
+    next();
+
+})
+
+
 //static assets middleware
 const path = require('path');
 app.use(express.static(path.join(__dirname,'public')));
@@ -9,6 +33,7 @@ app.use(express.static(path.join(__dirname,'public')));
 //Logging middleware
 const logger = require('morgan');
 const exp = require('constants');
+const { response } = require('express');
 app.use(logger('dev'));
 
 //ejs view engine
@@ -16,12 +41,33 @@ app.set('view engine','ejs');
 app.set('views','views');
 
 
+//homepage
+app.get('/',(req,res) =>{
+    res.render('./home')
+})
+
+//sign in page
+app.get('/signIn',(req,res) =>{
+    res.render('./signIn')
+})
 
 
 
+//sign in
+app.post('/sign_in',(req,res) =>{
 
+    const COOKIE_MAX_AGE = 1000*60*60*24;
+    const username = req.body.username;
+    res.cookie('username',username,{maxAge:COOKIE_MAX_AGE});
+    res.redirect('/')
 
+})
 
+//sign out
+app.post('/sign_out',(req,res) =>{
+    res.clearCookie('username');
+    res.redirect('/');
+})
 
 
 //server
